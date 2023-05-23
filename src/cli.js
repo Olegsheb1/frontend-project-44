725e1f71dd3e2d863b9de0545befb31e84d35010
Olegsheb1 <oleg.shebanov@bk.ru>
2023-05-23 12:46:57 +0300

    soon.

--- src/cli.js
+++ src/cli.js
@@ -4,3 +4,5 @@ export default function askForName() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
+
+export const getRandomNumber = (number = 100) => Math.floor(Math.random() * number);
